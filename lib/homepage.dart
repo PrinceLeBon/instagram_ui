05269de205209c1d2a0fe7404ef653e3c089ab87
PrinceLeBon <jeffersonhoundjo@gmail.com>
2023-03-23 13:07:06 +0100

    Update readme file

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -24,6 +24,7 @@ class _HomePageState extends State<HomePage> {
 
   int _Random() => Random().nextInt(100);
   late final int sms_number;
+  late final int sms_number1;
   late final List<Widget> _children = [
     UserHome(smsNumber: sms_number),
     const UserSearch(),
